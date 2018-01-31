0bcf63fe0babf39000f7e129953dd696707a22ad
Josh Winter <winterjoshr@gmail.com>
2018-01-30 23:33:58 -0500

    add icon for windows version of server

--- fishtank-server/winbuild.bat
+++ fishtank-server/winbuild.bat
@@ -1,1 +1,2 @@
-cl /EHsc /std:c++14 *.cpp object/*.cpp ../jni/network.cpp ws2_32.lib /link /out:fishtank-server.exe
+rc resource.rc
+cl /EHsc /std:c++14 *.cpp object/*.cpp ../jni/network.cpp ws2_32.lib resource.res /link /out:fishtank-server.exe
